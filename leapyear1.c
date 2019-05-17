2cf3882a2ca384d2492026d783e1d7d4294feb76
Famena <49779137+Famena@users.noreply.github.com>
2019-05-17 17:27:59 +0530

    Create leapyear1.c

---
+++ leapyear1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int year;
+printf("Input:");
+scanf("%d",&year);
+if(year%4==0)
+{
+printf("Output:Yes",year);
+}
+else
+{
+printf("Output:No",year);
+}
+return 0;
+}
